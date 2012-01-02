c365045166f79f2e52057d2119965c30fbed4f48
Mitsunori Komatsu <komamitsu@gmail.com>
2012-01-03 02:09:24 +0900

    remove unused annotation

--- src/test/java/com/komamitsu/addressbook/repository/impl/TestSqlMapPersonDaoImpl.java
+++ src/test/java/com/komamitsu/addressbook/repository/impl/TestSqlMapPersonDaoImpl.java
@@ -3,15 +3,12 @@ package com.komamitsu.addressbook.repository.impl;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.komamitsu.addressbook.repository.PersonDao;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/applicationContext-test.xml")
 public class TestSqlMapPersonDaoImpl extends AbstractTransactionalJUnit4SpringContextTests {
   @Autowired
